11f3137ee8ad7d3a618e6196968bd067ae40c0bf
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2023-06-14 16:40:03 +0900

    add list_logpush_jobs

---
+++ list_logpush_jobs.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ $# != 0 ]; then
+  echo
+  echo "$0"
+  echo
+  exit 1
+fi
+
+#======= Parameter Set =======
+
+EMAIL=${CF_EMAIL}
+APIKEY=${CF_APIKEY}
+ACCOUNT_ID=${CF_ACCOUNT_ID}
+API_TYPE='accounts'
+
+echo
+echo "EMAIL = ${EMAIL}"
+echo "ACCOUNT_ID = ${ACCOUNT_ID}"
+
+#======= Run Command =======
+COMMAND="curl -s \"https://api.cloudflare.com/client/v4/$API_TYPE/$ACCOUNT_ID/logpush/jobs\" -X GET \
+-H \"X-Auth-Email: $EMAIL\" \
+-H \"X-Auth-Key: $APIKEY\" | jq ."
+
+echo "COMMAND = ${COMMAND}"
+
+eval ${COMMAND}
